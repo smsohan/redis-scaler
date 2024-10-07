53bb79b0cc5af12f716bf45a4cfd5b4db3a16e14
Sohan SM <sohansm@google.com>
2024-10-07 17:57:53 +0000

    refactor

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 IMAGE=us-central1-docker.pkg.dev/sohansm-project/cloud-run-source-deploy/redis-scaler
+CONSUMER_SERVICE=redis-consumer
 gcloud builds submit --pack image=$IMAGE
 
 gcloud run deploy redis-scaler \
---set-env-vars REDIS_HOST=10.126.55.187,REDIS_PORT=6379,REDIS_LIST_NAME=mylist,REDIS_LIST_LENGTH=100,MAX_INSTANCE_COUNT=50,CONSUMER_PROJECT_ID=sohansm-project,CONSUMER_REGION=us-central1,CONSUMER_SERVICE_NAME=redis-scaler \
+--set-env-vars REDIS_HOST=10.126.55.187,REDIS_PORT=6379,REDIS_LIST_NAME=mylist,REDIS_LIST_LENGTH=100,MAX_INSTANCE_COUNT=10,CONSUMER_PROJECT_ID=sohansm-project,CONSUMER_REGION=us-central1,CONSUMER_SERVICE_NAME=$CONSUMER_SERVICE \
 --region us-central1 \
 --network=crf-vpc \
 --subnet=crf-vpc \
@@ -10,7 +11,7 @@ gcloud run deploy redis-scaler \
 --no-allow-unauthenticated \
 --image $IMAGE
 
-gcloud run deploy redis-consumer \
+gcloud run deploy $CONSUMER_SERVICE \
 --set-env-vars MODE=CONSUMER,REDIS_HOST=10.126.55.187,REDIS_PORT=6379,REDIS_LIST_NAME=mylist,REDIS_LIST_LENGTH=100,REDIS_CONSUMPTION_TIME_MILS=100 \
 --region us-central1 \
 --network=crf-vpc \
